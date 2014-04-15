a753dd566559967480b530516f9e53244c219c20
Clayton Meeker <clayton.meeker@improvingenterprises.com>
2014-04-15 12:52:22 -0500

    first command

--- Adventure/Adventure/Program.cs
+++ Adventure/Adventure/Program.cs
@@ -10,6 +10,15 @@ namespace Adventure
     {
         static void Main(string[] args)
         {
+            string input = "";
+            do
+            {
+                if (input.ToLower().Trim() == "dance")
+                    Console.WriteLine("You dance around like a fool");
+                Console.Write("> ");
+                input = Console.ReadLine();
+            }
+            while (input.ToLower().Trim() != "exit");
         }
     }
 }
