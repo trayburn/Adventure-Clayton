19dee75ed206303c3a11cd5538273646f3340a9f
Clayton Meeker <clayton.meeker@improvingenterprises.com>
2014-04-17 19:27:29 -0500

    shoot and pass working

--- Adventure/Adventure/Program.cs
+++ Adventure/Adventure/Program.cs
@@ -15,6 +15,12 @@ namespace Adventure
             {
                 if (input.ToLower().Trim() == "dance")
                     Console.WriteLine("You dance around like a fool");
+                
+                if (input.ToLower().Trim() == "pass")
+                    Console.WriteLine("Good Pass!");
+               
+                if (input.ToLower().Trim() == "shoot")
+                    Console.WriteLine("Off the mark. Try again? Choose yes or no");
                 Console.Write("> ");
                 input = Console.ReadLine();
             }
