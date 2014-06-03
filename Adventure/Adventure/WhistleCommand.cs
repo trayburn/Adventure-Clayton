d2da18e7ef1852ad25d3a82a52b917439df8a423
Tim Rayburn <tim@timrayburn.net>
2014-06-02 19:36:25 -0500

    Whistle

---
+++ Adventure/Adventure/WhistleCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventure
+{
+    public class WhistleCommand : BaseCommand, ICommand
+    {
+        public bool IsValid(string input)
+        {
+            return IsFirstWord("whistle", input);
+        }
+
+        public void Execute(string input)
+        {
+            Console.WriteLine("You whistle while you work.");
+        }
+    }
+
+}
